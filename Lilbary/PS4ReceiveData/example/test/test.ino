916f4d655cb6f8cfe4d369d108653f9cf0894a44
Sutthikan panla <sutigran2557@gmail.com>
2022-09-03 01:46:32 +0700

    ds4 contorl robot

--- Lilbary/PS4ReceiveData/example/test/test.ino
+++ Lilbary/PS4ReceiveData/example/test/test.ino
@@ -26,10 +26,43 @@ struct Remote
 
 Remote controller_data;
 
-void setRemote()
+void setup()
 {
-  PS4.begin("7c:9e:bd:48:4f:5a");
+    Serial.begin(115200);
+    Serial.println("Hello world");
+    PS4.begin("7c:9e:bd:48:4f:5a");
 }
+
+void loop()
+{
+    getRemote();
+    Serial.print("LX:");
+    Serial.print(controller_data.L_X);
+    Serial.print(" LY:");
+    Serial.print(controller_data.L_Y);
+    Serial.print(" RX:");
+    Serial.print(controller_data.R_X);
+    Serial.print(" RY:");
+    Serial.print(controller_data.R_Y);
+    Serial.print(" L1:");
+    Serial.print(controller_data.L1);
+    Serial.print(" L2:");
+    Serial.print(controller_data.L2);
+    Serial.print(" L3:");
+    Serial.print(controller_data.L3);
+    Serial.print(" R1:");
+    Serial.print(controller_data.R1);
+    Serial.print(" R2:");
+    Serial.print(controller_data.R2);
+    Serial.print(" R3:");
+    Serial.print(controller_data.R3);
+    Serial.print(" Opt:");
+    Serial.print(controller_data.Option);
+    Serial.print(" Sha:");
+    Serial.print(controller_data.Share);
+    Serial.println();
+}
+
 void getRemote()
 {
   controller_data.L_X = PS4.LStickX();
