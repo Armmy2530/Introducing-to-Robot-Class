11f6726ed3e14f45715e8555a84de0952179d469
Sutthikan panla <sutigran2557@gmail.com>
2022-09-20 09:37:11 +0700

    add calibrate sensor for esp32

--- esp32/main/pid/main.ino
+++ esp32/main/pid/main.ino
@@ -1,6 +1,7 @@
 #include <motor_esp32.h>
 #include <pwmWrite.h>
 #include <PS4Controller.h>
+
 // Motor L
 int enA = 27;
 int in1 = 26;
@@ -10,28 +11,22 @@ int enB = 13;
 int in3 = 14;
 int in4 = 12;
 
-float current_L = 0, pev_L = 0, target_L = 0;
-float current_R = 0, pev_R = 0, target_R = 0;
-float freerange = 17;
-// drive mode 2 setting
-int min_percentage = 25;
-int max_percentage = 125;
-
 Motor R_motor(in3, in4, enB);
 Motor L_motor(in1, in2, enA);
 
 // sensor_pin
-int L2_pin = 3, L2_value = 0;
-int L1_pin = 4, L1_value = 0;
-int C_pin = 5, C_value = 0;
-int R1_pin = 6, R1_value = 0;
-int R2_pin = 7, R2_value = 0;
+int L2_pin = 33, L2_value = 0;
+int L1_pin = 32, L1_value = 0;
+int C_pin = 35, C_value = 0;
+int R1_pin = 34, R1_value = 0;
+int R2_pin = 39, R2_value = 0;
 
-int turn_ms = 20;
-int fd_speed = 80;
-int turn_speed = 20;
+int ref_sensor[5] = {1000, 2350, 2948, 2748, 2348};
+boolean sensor_bool[5] = {1, 1, 1, 1, 1};
+int sensor_val[5] = {1, 1, 1, 1, 1};
+int dif_ref = 500;
 
-int baseSpeed = 80;
+int baseSpeed = 150;
 int maxSpeed = 255;
 int motorSpeed;
 int leftSpeed, rightSpeed;
@@ -40,132 +35,215 @@ int Kd = 1;
 float Ki = 00.0000001;
 int error = 0, pre_error = 0, sum_error;
 
-bool W(int n)
-{
-    if (n == 1)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-bool B(int n)
-{
-    if (n == 0)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-Motor L_motor(in1, in2, enA);
-Motor R_motor(in3, in4, enB);
+
+int turn_ms = 20;
+int fd_speed = 80;
+int turn_speed = 20;
+
+int ref = 2500;
 
 void setup()
 {
     Serial.begin(115200);
-    Serial.println("Hello world");
-    pinMode(3, INPUT);
-    pinMode(4, INPUT);
-    pinMode(5, INPUT);
-    pinMode(6, INPUT);
-    pinMode(7, INPUT);
+    calibrate_sensor(5);
+    delay(10000);
+    readSensor();
+    trackline_R(100,1,50);
+    tr_sensor(150,0);
+    trackline_Cross(100,5,50);
+    tl_sensor(0,150);
+    m(100,100);
     delay(500);
-    trackline_R(60, 1);
-    tr_sensor(80, 40);
-    trackline_R(60, 7);
-    fd(60,60);
-    delay(400);
-    m(0,-100);
-    delay(100);
-    while(!(W(L2_value) && W(L1_value) && W(C_value) && B(R1_value) && W(R2_value))){
-        readSensor();
-        m(0,-100);
-    }
-    trackline_Cross(80,1);
-    m(70,70);
-    delay(500);
-    stop(false);
+    m(0,0);
 }
-
 void loop()
 {
+}
+void readSensor()
+{
+    sensor_val[0] = analogRead(L2_pin);
+    sensor_val[1] = analogRead(L1_pin);
+    sensor_val[2] = analogRead(C_pin);
+    sensor_val[3] = analogRead(R1_pin);
+    sensor_val[4] = analogRead(R2_pin);
+    for (int n = 0; n <= 4; n++)
+    {
+        sensor_bool[n] = W(sensor_val[n], ref_sensor[n]);
+    }
+    L2_value = int(sensor_bool[0]);
+    L1_value = int(sensor_bool[1]);
+    C_value  = int(sensor_bool[2]);
+    R1_value = int(sensor_bool[3]);
+    R2_value = int(sensor_bool[4]);
+}
+
+void calibrate_sensor(int n_check)
+{
+    readSensor();
+    int white_val[5] = {0, 0, 0, 0, 0};
+    int black_val[5] = {0, 0, 0, 0, 0};
+    int avg_val[5] = {0, 0, 0, 0, 0};
+    int confirm_black = 0;
+    Serial.println("Start Calibrat sensor ..........");
+    for (int i = 0; i < n_check; i++)
+    {
+        for (int n = 0; n <= 4; n++)
+        {
+            readSensor();
+            white_val[n] = sensor_val[n];
+        }
+        m(120, 120);
+        while (true)
+        {
+            readSensor();
+            for (int i = 0; i <= 4; i++)
+            {
+                readSensor();
+                if (abs(sensor_val[i] - white_val[i]) >= dif_ref)
+                {
+                    confirm_black++;
+                }
+            }
+            if (confirm_black == 5)
+            {
+                for (int n = 0; n <= 4; n++)
+                {
+                    black_val[n] = sensor_val[n];
+                }
+                delay(50);
+                m(0, 0);
+                confirm_black = 0;
+                break;
+            }
+            else
+                confirm_black = 0;
+        }
+        for (int n = 0; n <= 4; n++)
+        {
+            if (avg_val[n] = 0)
+            {
+                avg_val[n] = (white_val[n] + black_val[n]) / 2;
+            }
+            else
+            {
+                avg_val[n] = avg_val[n] + ((white_val[n] + black_val[n]) / 2) / 2;
+            }
+        }
+        Serial.print("loop: ");
+        Serial.print(i);
+        for (int n = 0; n <= 4; n++)
+        {
+            Serial.print(" ");
+            Serial.print(avg_val[n]);
+        }
+        Serial.println();
+        while (true)
+        {
+            readSensor();
+            // for (int n = 0; n <= 4; n++)
+            // {
+            //     Serial.print(avg_val[n]);
+            //     Serial.print(" ");
+            //     Serial.print(sensor_val[n]);
+            //     Serial.print(" ");
+            //     Serial.print(W(sensor_val[n],avg_val[n]));
+            //     Serial.print(" ");
+            // }
+            if((W(sensor_val[0], avg_val[0]) && W(sensor_val[1], avg_val[1]) && W(sensor_val[2], avg_val[2]) && W(sensor_val[3], avg_val[3]) && W(sensor_val[4], avg_val[4]))){Serial.print("True ");break;}
+            // Serial.println();
+            m(-100, -100);
+        }
+        delay(500);
+        m(0, 0);
+        delay(1000);
+    }
+    for (int n = 0; n <= 4; n++)
+    {
+        ref_sensor[n] = avg_val[n];
+        Serial.print("Final Ref");
+        Serial.print(" ");
+        Serial.print(ref_sensor[n]);
+    }
+    Serial.println();
 }
 
 void m(int l, int r)
 {
+    if (l > 255)
+    {
+        l = 255;
+    }
+    if (r > 255)
+    {
+        r = 255;
+    }
     L_motor.m(l);
     R_motor.m(r);
 }
 
-void readSensor()
+bool W(int n, int ref)
 {
-    L2_value = digitalRead(L2_pin);
-    L1_value = digitalRead(L1_pin);
-    C_value = digitalRead(C_pin);
-    R1_value = digitalRead(R1_pin);
-    R2_value = digitalRead(R2_pin);
-}
-
-void trackline_1(int fd_speed, int turn_speed, int turn_ms)
-{
-    readSensor();
-    if (L1_value == 1 && R1_value == 1)
+    if (n > ref)
     {
-        fd(fd_speed, fd_speed);
-    }
-    else if (L1_value == 0 && R1_value == 1)
-    {
-        sl(turn_speed, turn_speed);
-        delay(turn_ms);
-    }
-    else if (L1_value == 1 && R1_value == 0)
-    {
-        sr(turn_speed, turn_speed);
-        delay(turn_ms);
+        return true;
     }
     else
     {
-        stop(false);
+        return false;
+    }
+}
+bool B(int n, int ref)
+{
+    if (n < ref)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
     }
 }
 
 void trackline_pid()
 {
     readSensor();
-    if (W(L2_value) && W(L1_value) && W(C_value) && B(R1_value))
+    if (W(L2_value) && W(L1_value) && W(C_value) && W(R1_value) && B(R2_value))
     {
         error = 4; // w w w w b
     }
-    else if (W(L2_value) && W(L1_value) && B(C_value) && B(R1_value))
+    else if (W(L2_value) && W(L1_value) && W(C_value) && B(R1_value) && B(R2_value))
     {
         error = 3; // w w w b b
     }
-    else if (W(L2_value) && W(L1_value) && B(C_value) && W(R1_value))
+    else if (W(L2_value) && W(L1_value) && W(C_value) && B(R1_value) && W(R2_value))
     {
         error = 2; // w w w b w
     }
-    else if (W(L2_value) && B(L1_value) && B(C_value) && W(R1_value))
+    else if (W(L2_value) && W(L1_value) && B(C_value) && B(R1_value) && W(R2_value))
     {
         error = 1; // w w b b w
     }
-    else if (W(L2_value) && B(L1_value) && W(C_value) && W(R1_value))
+    else if (W(L2_value) && W(L1_value) && B(C_value) && W(R1_value) && W(R2_value))
     {
         error = 0; // w w b w w
     }
-    else if (B(L2_value) && B(L1_value) && W(C_value) && W(R1_value))
+    else if (W(L2_value) && B(L1_value) && B(C_value) && W(R1_value) && W(R2_value))
     {
         error = -1; // w b b w w
     }
-    else if (B(L2_value) && W(L1_value) && W(C_value) && W(R1_value))
+    else if (W(L2_value) && B(L1_value) && W(C_value) && W(R1_value) && W(R2_value))
     {
         error = -2; // w b w w w
     }
-    else if (W(L2_value) && W(L1_value) && W(C_value) && W(R1_value))
+    else if (B(L2_value) && B(L1_value) && W(C_value) && W(R1_value) && W(R2_value))
+    {
+        error = -3; // b b w w w
+    }
+    else if (B(L2_value) && W(L1_value) && W(C_value) && W(R1_value) && W(R2_value))
+    {
+        error = -4; // b w w w w
+    }
+    else if (W(L2_value) && W(L1_value) && W(C_value) && W(R1_value) && W(R2_value))
     {
         error = pre_error; // w w w w w
     }
@@ -186,7 +264,7 @@ void trackline_pid()
     pre_error = error;
     sum_error += error;
 }
-void trackline_R(int turn_speed, int line)
+void trackline_R(int turn_speed, int line,int ms_delay)
 {
     int count = 0;
     while (count < line)
@@ -206,10 +284,11 @@ void trackline_R(int turn_speed, int line)
             }
         }
     }
+    delay(ms_delay);
     stop(false);
 }
 
-void trackline_L(int turn_speed, int line)
+void trackline_L(int turn_speed, int line,int ms_delay)
 {
     int count = 0;
     while (count < line)
@@ -229,21 +308,22 @@ void trackline_L(int turn_speed, int line)
             }
         }
     }
+    delay(ms_delay);
     stop(false);
 }
 
-void trackline_Cross(int turn_speed, int line)
+void trackline_Cross(int turn_speed, int line,int ms_delay)
 {
     int count = 0;
     while (count < line)
     {
         trackline_pid();
-        if (B(R1_value) && B(L1_value))
+        if (B(R2_value) && B(L2_value))
         {
             delay(100);
-            if (R1_value == 0 && L1_value == 0)
+            if (R2_value == 0 && L2_value == 0)
             {
-                while (L1_value == 0 && R1_value == 0)
+                while (L2_value == 0 && R2_value == 0)
                 {
                     fd(fd_speed, fd_speed);
                     readSensor();
@@ -252,6 +332,7 @@ void trackline_Cross(int turn_speed, int line)
             }
         }
     }
+    delay(ms_delay);
     stop(false);
 }
 
@@ -268,6 +349,26 @@ void tr_sensor(int L_speed, int R_speed)
     while (C_value == 1)
     {
         sr(L_speed, R_speed);
+        readSensor();
+    }
+    delay(100);
+    stop(false);
+}
+
+
+void tl_sensor(int L_speed, int R_speed)
+{
+    fd(L_speed, R_speed);
+    delay(100);
+    stop(false);
+    delay(100);
+    sl(L_speed, R_speed);
+    delay(400);
+    stop(false);
+    readSensor();
+    while (C_value == 1)
+    {
+        sl(L_speed, R_speed);
         readSensor();
     }
     delay(100);
--- prin/mainmission/trackline/main/main.ino
+++ prin/mainmission/trackline/main/main.ino
@@ -5,7 +5,7 @@
 // Motor L
 int enA = 27;
 int in1 = 26;
-int in2 = 25;
+int in2 = 33;
 // Motor R
 int enB = 13;
 int in3 = 14;
@@ -15,15 +15,16 @@ Motor R_motor(in3, in4, enB);
 Motor L_motor(in1, in2, enA);
 
 // sensor_pin
-int L2_pin = 33, L2_value = 0;
-int L1_pin = 32, L1_value = 0;
-int C_pin = 35, C_value = 0;
-int R1_pin = 34, R1_value = 0;
-int R2_pin = 39, R2_value = 0;
+int L2_pin = 25;
+int L1_pin = 32;
+int C_pin  = 35;
+int R1_pin = 34;
+int R2_pin = 39;
 
 int ref_sensor[5] = {1000, 2350, 2948, 2748, 2348};
-boolean sensor_bool[5] = {1,1,1,1,1};
-int sensor_val[5] = {1,1,1,1,1};
+boolean sensor_bool[5] = {1, 1, 1, 1, 1};
+int sensor_val[5] = {1, 1, 1, 1, 1};
+int dif_ref = 300;
 
 int turn_ms = 20;
 int fd_speed = 80;
@@ -33,16 +34,24 @@ int ref = 2500;
 
 void setup()
 {
+    pinMode(L2_pin,INPUT);
+    pinMode(L1_pin,INPUT);
+    pinMode(C_pin,INPUT);
+    pinMode(R1_pin,INPUT);
+    pinMode(R2_pin,INPUT);
     Serial.begin(115200);
+    calibrate_sensor(5);
 }
 void loop()
 {
     readSensor();
-    for(int n = 0; n<=5 ; n++){
+    for (int n = 0; n <= 4; n++)
+    {
         Serial.print(sensor_val[n]);
         Serial.print(" ");
     }
-    for(int n = 0; n<=5 ; n++){
+    for (int n = 0; n <= 4; n++)
+    {
         Serial.print(sensor_bool[n]);
     }
     Serial.println();
@@ -54,19 +63,127 @@ void readSensor()
     sensor_val[2] = analogRead(C_pin);
     sensor_val[3] = analogRead(R1_pin);
     sensor_val[4] = analogRead(R2_pin);
-    for(int n = 0; n<=5 ; n++){
-        sensor_bool[n] = W(sensor_val[n],ref_sensor[n]);
+    for (int n = 0; n <= 4; n++)
+    {
+        sensor_bool[n] = W(sensor_val[n], ref_sensor[n]);
     }
 }
+
+void calibrate_sensor(int n_check)
+{
+    readSensor();
+    int white_val[5] = {0, 0, 0, 0, 0};
+    int black_val[5] = {0, 0, 0, 0, 0};
+    int avg_val[5] = {0, 0, 0, 0, 0};
+    int confirm_black = 0;
+    Serial.println("Start Calibrat sensor ..........");
+    for (int i = 0; i < n_check; i++)
+    {
+        for (int n = 0; n <= 4; n++)
+        {
+            readSensor();
+            white_val[n] = sensor_val[n];
+        }
+        m(120, 120);
+        while (true)
+        {
+            readSensor();
+            for (int n = 0; n <= 4; n++)
+            {
+                Serial.print(sensor_val[n]);
+                Serial.print(" ");
+            }
+            Serial.println(" ");
+            for (int i = 0; i <= 4; i++)
+            {
+                readSensor();
+                if (abs(sensor_val[i] - white_val[i]) >= dif_ref)
+                {
+                    confirm_black++;
+                }
+            }
+            if (confirm_black == 5)
+            {
+                for (int n = 0; n <= 4; n++)
+                {
+                    black_val[n] = sensor_val[n];
+                }
+                delay(50);
+                m(0, 0);
+                confirm_black = 0;
+                break;
+            }
+            else
+                confirm_black = 0;
+        }
+        for (int n = 0; n <= 4; n++)
+        {
+            if (avg_val[n] = 0)
+            {
+                avg_val[n] = (white_val[n] + black_val[n]) / 2;
+            }
+            else
+            {
+                avg_val[n] = avg_val[n] + ((white_val[n] + black_val[n]) / 2) / 2;
+            }
+        }
+        Serial.print("loop: ");
+        Serial.print(i);
+        for (int n = 0; n <= 4; n++)
+        {
+            Serial.print(" ");
+            Serial.print(avg_val[n]);
+        }
+        Serial.println();
+        while (true)
+        {
+            readSensor();
+            // for (int n = 0; n <= 4; n++)
+            // {
+            //     Serial.print(avg_val[n]);
+            //     Serial.print(" ");
+            //     Serial.print(sensor_val[n]);
+            //     Serial.print(" ");
+            //     Serial.print(W(sensor_val[n],avg_val[n]));
+            //     Serial.print(" ");
+            // }
+            if ((W(sensor_val[0], avg_val[0]) && W(sensor_val[1], avg_val[1]) && W(sensor_val[2], avg_val[2]) && W(sensor_val[3], avg_val[3]) && W(sensor_val[4], avg_val[4])))
+            {
+                Serial.print("True ");
+                break;
+            }
+            // Serial.println();
+            m(-100, -100);
+        }
+        delay(500);
+        m(0, 0);
+        delay(1000);
+    }
+    for (int n = 0; n <= 4; n++)
+    {
+        ref_sensor[n] = avg_val[n];
+        Serial.print("Final Ref");
+        Serial.print(" ");
+        Serial.print(ref_sensor[n]);
+    }
+    Serial.println();
+}
+
 void m(int l, int r)
 {
-    if(l > 255){l = 255;}
-    if(r > 255){r = 255;}
+    if (l > 255)
+    {
+        l = 255;
+    }
+    if (r > 255)
+    {
+        r = 255;
+    }
     L_motor.m(l);
     R_motor.m(r);
 }
 
-bool W(int n,int ref)
+bool W(int n, int ref)
 {
     if (n > ref)
     {
@@ -77,7 +194,7 @@ bool W(int n,int ref)
         return false;
     }
 }
-bool B(int n,int ref)
+bool B(int n, int ref)
 {
     if (n < ref)
     {
